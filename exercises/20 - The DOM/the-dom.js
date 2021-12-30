4db117090817c756793a25c12fb9dad8aee4269f
mattyImry <matteo.imeri@gmail.com>
2021-12-30 12:54:34 +0000

    Started dom exercise

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,6 @@
+const par = document.querySelector('p');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+const heading = document.querySelector('h2');
+console.log(item2Image);
+
