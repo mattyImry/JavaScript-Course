ab004911488a57c366a04334b42d6a81c55485d3
mattyImry <matteo.imeri@gmail.com>
2022-01-04 19:02:02 +0000

    Dom classList

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -2,5 +2,30 @@ const par = document.querySelector('p');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
 const heading = document.querySelector('h2');
-console.log(item2Image);
+heading.textContent = 'Ostia';
+console.log(heading.textContent);
+console.log(heading.innerText);
+
+
+const steakList = document.querySelector('.steak');
+console.log(steakList.textContent);
+
+//steakList.textContent = `${steakList.textContent} &#129385;`
+
+steakList.insertAdjacentText('beforeend', 'ostia')
+
+const pic = document.querySelector(".nice");
+
+
+// pic.classList.add('open');
+// pic.classList.remove('open');
+
+// pic.classList.add('round');
+//pic.classList.toggle('round');
+
+function toggleRound() {
+    pic.classList.toggle('round')
+}
+
+pic.addEventListener('click', toggleRound);
 
