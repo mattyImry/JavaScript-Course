237c434b6b0d8005689121bbb1553e0615394c48
mattyImry <matteo.imeri@gmail.com>
2022-01-20 17:40:50 +0000

    Completed scroll to accept exe

---
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,23 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallBack(payLoad) {
+    if (payLoad[0].intersectionRatio === 1) {
+        button.disabled = false;
+        // stop observing the button
+        ob.unobserve(terms.lastElementChild);
+
+    // add this code to reset when off sacroll
+    // } else {
+    //     button.disabled = true;
+     }
+
+}
+
+const ob = new IntersectionObserver(obCallBack, {
+    root: terms,
+    threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
